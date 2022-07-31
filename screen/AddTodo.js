36d6be1c93fb2eadd6b0460b4be9a7bd7b47f5ea
brianbacca <brianbn05@gmai.com>
2022-07-30 22:52:42 -0300

    add DateTimePicker in component AddTodo

--- screen/AddTodo.js
+++ screen/AddTodo.js
@@ -1,10 +1,101 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Switch,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function AddTodo() {
+  const [name, setName] = React.useState("");
+  const [date, setDate] = React.useState(new Date());
+  const [isToday, setIsToday] = React.useState(false);
   return (
-    <View>
-      <Text>this is the addTodo</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Add Task</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Name</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Task"
+          placeholderTextColor="#00000030"
+          onChangeText={(text) => {
+            setName(text);
+          }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Hour</Text>
+        <DateTimePicker
+          value={date}
+          mode={"time"}
+          is24Hour={true}
+          onChange={(event, selectedDate) => setDate(selectedDate)}
+          style={{ width: "80%" }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Today</Text>
+        <Switch
+          value={isToday}
+          onValueChange={(value) => {
+            setIsToday(value);
+          }}
+        />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={{ color: "white" }}>Done</Text>
+      </TouchableOpacity>
+      <Text style={styles.inputTitle}>Alert</Text>
+      <Text style={{ color: "#00000040", fontSize: 12, maxWidth: "85%" }}>
+        You will receive an alert at the time you set for this reminder
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F7F8FA",
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: "bold",
+    marginBottom: 35,
+    marginTop: 10,
+  },
+  inputTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    lineHeight: 24,
+  },
+  textInput: {
+    borderBottomColor: "#00000030",
+    borderBottomWidth: 1,
+    width: "80%",
+  },
+  inputContainer: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingBottom: 30,
+  },
+  button: {
+    marginTop: 30,
+    marginBottom: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000000",
+    height: 46,
+    borderRadius: 11,
+  },
+  inputTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    lineHeight: 24,
+  },
+});
